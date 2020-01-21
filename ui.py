b1ede2d2f52bef2e0806f14b81ceb70a9b66b907
Franciszek Jaworski <franciszekjaworski@gmail.com>
2020-01-21 13:36:39 +0100

    change print_table and column_width_counter to version from our previous project
    Add column_width_counter that takes:
    - label_list: list of titles for a table
    - table: list of list
    Returns:
    - list of column widths (the smallest possible that will encompass words from table and label_list)

--- ui.py
+++ ui.py
@@ -1,6 +1,20 @@
 """ User Interface (UI) module """
 import data_manager
 
+
+def column_width_counter(label_list, table):
+    columns_width = []
+    for column in range(len(label_list)):
+        max = 0
+        for record in range(len(table)):
+            
+            if len(table[record][column]) >= max:
+                max = len(table[record][column])    
+            
+        columns_width.append(max)
+    return columns_width
+
+    
 def print_table(table, title_list):
     """
     Prints table with data.
@@ -21,67 +35,31 @@ def print_table(table, title_list):
     Returns:
         None: This function doesn't return anything it only prints to console.
     """
-    dash_char = "-"
-    max_len_column = [0,0,0,0,0,0,0]
-    column_counter = 0
+    table.insert(0, title_list)
 
-    #maximum length of every element in title data base (max_len_column)
-    for rows in title_list:
-        if len(rows) > max_len_column[column_counter]:
-            max_len_column[column_counter] = len(rows)
-            column_counter += 1
+    # getting column width for a table
+    columns_width = column_width_counter(title_list, table)
 
-    #maximum Length of every row in table data base. (max_len_column)
-    for rows in table:
-        column_counter = 0
-        for column in rows:
-            if len(column) > max_len_column[column_counter]:
-                max_len_column[column_counter] = len(column)
-            column_counter += 1
+    edges_control = 1
+    table_width = len(columns_width) - edges_control
+    for item in columns_width:
+        table_width += item
 
-    #header
-    print("/",end = '')
-    counter = 0
-    for i in max_len_column:
-        if i != 0:
-            counter += 1
-            if counter == len(title_list):
-                print(f"{dash_char * (i + 1)}\\",end = '')
-            else:
-                print(f"{dash_char * (i + 1)}|",end = '')
-    print("")
+    break_line = '|'
+    for num in columns_width:
+        break_line += num*'-' + '|'
 
-
-    #title
-    counter = 0
-    for title in title_list:
-        print(f'| {title.center(max_len_column[counter])}', end = "")
-        counter += 1
-    print("|")
-
-    #data
-    for rows in table:
-        counter = 0
-        for i in max_len_column:
-            if i != 0:
-                print(f"|{dash_char * (i + 1)}", end = '')
-        print("|")
-        for column in rows:
-            print(f"| {column.center(max_len_column[counter])}", end = "")
-            counter += 1
-        print('|')
-
-    #footer
-    print("\\",end = '')
-    counter = 0
-    for i in max_len_column:
-        if i != 0:
-            counter += 1
-            if counter == len(title_list):
-                print(f"{dash_char * (i + 1)}/",end = '')
-            else:
-                print(f"{dash_char * (i + 1)}|",end = '')
-
+    print('/' + "-" * table_width + '\\')
+    for position in table[:-1]:
+        print("|", end="")
+        for position_index, column in enumerate(position):
+            print("{0:^{1}}".format(column, columns_width[position_index]), end ="|")
+        print('')
+        print(break_line)
+    print("|", end="")
+    for position_index, column in enumerate(table[-1]):    
+        print("{0:^{1}}".format(column, columns_width[position_index]), end ="|")
+    print('\n' + '\\' + "-" * table_width + '/')
 
 def print_result(result, label):
     """
